ecb7b95a1a9c5e618bea29632b29b1c8880e0818
basharbakkar <basharinitial.bakkar@gmail.com>
2022-07-23 21:13:43 +0300

    create navbar.js

---
+++ frontend/src/components/Navbar/index.js
@@ -0,0 +1,23 @@
+
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+
+function TextLinkExample() {
+  return (
+    <Navbar>
+      <Container>
+        <Navbar.Brand href="#home">Posts</Navbar.Brand>
+        <Navbar.Brand href="#home">Users</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          
+          <Navbar.Text>
+            Signed in as: <a href="#login">Mark Otto</a>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default TextLinkExample;
